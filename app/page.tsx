313f740565809316129f1011aa5ac8a67ada47fc
Luewngier <g217881111@gmail.com>
2023-07-11 23:10:14 +0800

    style: add basic layout of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
+export interface Product {
+  id?: number
+  product: string
+  price: string
+}
+
 export default async function Home() {
-  return <main className=""></main>
+  const res = await fetch(
+    'https://64ad213bb470006a5ec56616.mockapi.io/products',
+    {
+      cache: 'no-cache',
+    }
+  )
+
+  const products: Product[] = await res.json()
+
+  return (
+    <main className="">
+      <h1 className="text-3xl font-bold text-center">Products Warehouse</h1>
+
+      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+        <input
+          type="text"
+          placeholder="Enter Product name..."
+          className="border border-gray-300 p-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder="Enter Price name..."
+          className="border border-gray-300 p-2 rounded-md"
+        />
+
+        <button className="bg-rose-500 text-white p-2 rounded-md border">
+          Add Product
+        </button>
+      </form>
+
+      <h2 className="font-bold p-5">List of Products</h2>
+
+      <div className="flex flex-wrap gap-5">
+        {products.map((product) => (
+          <div key={product.id} className="p-5 shadow">
+            <p>{product.product}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
