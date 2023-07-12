ba1e1a1af58bbb9318463feba4c17f88bcd4b82f
Luewngier <g217881111@gmail.com>
2023-07-12 10:52:31 +0800

    feat: implement add product feature in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { revalidateTag } from 'next/cache'
+
 export interface Product {
   id?: number
   product: string
@@ -9,23 +11,53 @@ export default async function Home() {
     'https://64ad213bb470006a5ec56616.mockapi.io/products',
     {
       cache: 'no-cache',
+      next: {
+        tags: ['products'],
+      },
     }
   )
 
   const products: Product[] = await res.json()
 
+  const addProductToDatabase = async (e: FormData) => {
+    'use server'
+
+    const product = e.get('product')?.toString()
+    const price = e.get('price')?.toString()
+
+    if (!product || !price) return
+
+    const newProduct: Product = {
+      product,
+      price,
+    }
+
+    await fetch('https://64ad213bb470006a5ec56616.mockapi.io/products', {
+      method: 'POST',
+      body: JSON.stringify(newProduct),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    revalidateTag('products')
+  }
+
   return (
     <main className="">
       <h1 className="text-3xl font-bold text-center">Products Warehouse</h1>
 
-      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+      <form
+        action={addProductToDatabase}
+        className="flex flex-col gap-5 max-w-xl mx-auto p-5"
+      >
         <input
-          type="text"
+          name="product"
           placeholder="Enter Product name..."
           className="border border-gray-300 p-2 rounded-md"
         />
         <input
-          type="text"
+          name="price"
           placeholder="Enter Price name..."
           className="border border-gray-300 p-2 rounded-md"
         />
